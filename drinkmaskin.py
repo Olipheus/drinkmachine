a73578ffc7f9bf258689739145edecfdc142ed67
Olipheus <wsdef@hotmail.com>
2017-10-05 07:32:10 +0200

    Update drinkmaskin.py
    Changed the initial output from LOW to HIGH and the output in respective drink. The signal seems to be inverted, since setting the initial output as LOW made the pumps run constantly, until given the signal LOW (or False)..
    
    I've trimmed the times to make good drinks, and also made two drinks that is merely booze.

--- drinkmaskin.py
+++ drinkmaskin.py
@@ -16,16 +16,18 @@ GPIO.setmode(GPIO.BCM)
 #List of output channels for airpumps set as output 0V
 output_list = (5, 6, 12, 13, 19, 16, 26, 20, 21)
 
+#NOTICE!!! Initial set to HIGH instead of LOW. Apparently the logic is somewhat inverted, and using initial LOW will enable
+#out signal instead of disable it, making the relays close it's circuits instead of leaving them open.
 
-GPIO.setup(5, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(6, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(12, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(13, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(19, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(16, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(26, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(20, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(21, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(5, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(6, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(12, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(13, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(19, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(16, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(26, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(20, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(21, GPIO.OUT, initial = GPIO.HIGH)
 
 #General times for booze and soda
 
@@ -52,58 +54,56 @@ from tkinter import *
 #Timer for drink 1
 
 def makeGrogg1():
-    GPIO.output(soda_1, True)
-    GPIO.output(liquor_1, True)
-    sprit = Timer(2.0, turnOffBooze1)
-    virke = Timer(4.0, turnOffSoda1)
+    GPIO.output(soda_1, False)
+    GPIO.output(liquor_1, False)
+    sprit = Timer(1.9, turnOffBooze1)
+    virke = Timer(8.5, turnOffSoda1)
     sprit.start()
     virke.start()
 def turnOffBooze1():
-    GPIO.output(liquor_1, False)
+    GPIO.output(liquor_1, True)
 def turnOffSoda1():
-    GPIO.output(soda_1, False)
+    GPIO.output(soda_1, True)
 
 #Timer for drink 2
 
 def makeGrogg2():
-    GPIO.output(soda_2, True)
-    GPIO.output(liquor_2, True)
-    sprit = Timer(2.0, turnOffBooze2)
-    virke = Timer(4.0, turnOffSoda2)
+    GPIO.output(soda_2, False)
+    GPIO.output(liquor_2, False)
+    sprit = Timer(1.9, turnOffBooze2)
+    virke = Timer(8.5, turnOffSoda2)
     sprit.start()
     virke.start()
 def turnOffBooze2():
-    GPIO.output(liquor_2, False)
+    GPIO.output(liquor_2, True)
 def turnOffSoda2():
-    GPIO.output(soda_2, False)
+    GPIO.output(soda_2, True)
 
-#Timer for drink 3
+#Timer for drink 3, booze only
 
 def makeGrogg3():
-    GPIO.output(soda_3, True)
-    GPIO.output(liquor_3, True)
-    sprit = Timer(2.0, turnOffBooze3)
-    virke = Timer(4.0, turnOffSoda3)
-    sprit.start()
-    virke.start()
-def turnOffBooze3():
-    GPIO.output(liquor_3, False)
-def turnOffSoda3():
-    GPIO.output(soda_3, False)
 
-#Timer for drink 4
+    GPIO.output(liquor_3, False)
+    sprit = Timer(1.9, turnOffBooze3)
+
+    sprit.start()
+
+def turnOffBooze3():
+    GPIO.output(liquor_3, True)
+def turnOffSoda3():
+
+#Timer for drink 4, booze only
 
 def makeGrogg4():
-    GPIO.output(soda_4, True)
-    GPIO.output(liquor_4, True)
-    sprit = Timer(2.0, turnOffBooze4)
-    virke = Timer(4.0, turnOffSoda4)
+    GPIO.output(liquor_4, False)
+
+    sprit = Timer(1.9, turnOffBooze4)
+
     sprit.start()
     virke.start()
 def turnOffBooze4():
-    GPIO.output(liquor_4, False)
-def turnOffSoda4():
-    GPIO.output(soda_4, False)
+    GPIO.output(liquor_4, True)
+
 
 
 
@@ -119,9 +119,9 @@ button1 = Button(mainframe, text = 'Rom o Cola', command = makeGrogg1, height = 
 button1.grid(row=1, column=0)
 button2 = Button(mainframe, text = 'Gin o Tonic', command = makeGrogg2, height = 8, width = 20)
 button2.grid(row=1, column=2)
-button3 = Button(mainframe, text = 'Screwdriver', command = makeGrogg3, height = 8, width = 20)
+button3 = Button(mainframe, text = 'Whiskey', command = makeGrogg3, height = 8, width = 20)
 button3.grid(row=2, column=0)
-button4 = Button(mainframe, text = 'Vodka o Fanta', command = makeGrogg4, height = 8, width = 20)
+button4 = Button(mainframe, text = 'Cognac', command = makeGrogg4, height = 8, width = 20)
 button4.grid(row=2, column=2)
  
  
